cdb038c72fd3d8e2cf47f43333686f3dbc73f8fe
Ana <ana.mandello@gmail.com>
2023-03-06 19:15:37 -0300

    Melhorias Storybook

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -9,6 +9,13 @@ export default {
     src: 'https://github.com/anamandello.png',
     alt: 'Ana Flávia',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
